031664a86e85c8ff74c19a1f21ee1b7ab74320ec
DeZoST <zosted62400@gmail.com>
2023-09-28 17:26:37 +0200

    Modifié le statut actif des input field et des tipButton, et raccourci le code

--- app.js
+++ app.js
@@ -1,55 +1,42 @@
-const bodyContainer = document.querySelector("body");
-const billContainer = document.getElementById("bill__container");
-const peopleContainer = document.getElementById("people__container");
-
 const billInput = document.getElementById("bill__input");
 const customInput = document.getElementById("custom__input");
 const peopleInput = document.getElementById("people__input");
 const tipAmountElement = document.getElementById("tip__amount");
 const totalAmountElement = document.getElementById("total__amount");
 const resetButton = document.getElementById("reset__button");
+const tipButtons = document.querySelectorAll(".tip__button");
+const containers = document.querySelectorAll(".input__container");
 
-
-let customTipActive = false;
+let selectedTipPercentage = 0;
 
 function calculateTip() {
   const billAmount = parseFloat(billInput.value) || 0;
   const customTipPercentage = parseFloat(customInput.value) || 0;
   const numberOfPeople = parseInt(peopleInput.value) || 1;
-  const selectedPercentage = customTipActive ? customTipPercentage : getSelectedDefaultPercentage();
 
-  const tipAmount = (billAmount * (selectedPercentage / 100)) / numberOfPeople;
+  selectedTipPercentage = customInput.value ? customTipPercentage : selectedTipPercentage;
+
+  const tipAmount = (billAmount * (selectedTipPercentage / 100)) / numberOfPeople;
   const totalAmount = billAmount / numberOfPeople + tipAmount;
 
-  tipAmountElement.textContent = "$" + tipAmount.toFixed(2);
-  totalAmountElement.textContent = "$" + totalAmount.toFixed(2);
-}
-
-function getSelectedDefaultPercentage() {
-  const activeButton = document.querySelector(".tip__button.active");
-  return parseFloat(activeButton?.textContent) || 0;
+  tipAmountElement.textContent = `$${tipAmount.toFixed(2)}`;
+  totalAmountElement.textContent = `$${totalAmount.toFixed(2)}`;
 }
 
 function handleTipButtonClick(button) {
-  customTipActive = false;
-  const buttons = document.querySelectorAll(".tip__button");
-  buttons.forEach((btn) => btn.classList.remove("active"));
+  tipButtons.forEach((btn) => btn.classList.remove("active"));
   button.classList.add("active");
+  selectedTipPercentage = parseFloat(button.textContent);
+  customInput.value = "";
   calculateTip();
 }
 
 billInput.addEventListener("input", calculateTip);
-customInput.addEventListener("input", () => {
-  customTipActive = true;
-  calculateTip();
-});
+customInput.addEventListener("input", calculateTip);
 peopleInput.addEventListener("input", calculateTip);
 
-const percentageButtons = document.querySelectorAll(".tip__button");
-percentageButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    handleTipButtonClick(button);
-  });
+tipButtons.forEach((button) => {
+  button.addEventListener("click", () => handleTipButtonClick(button));
 });
 
 resetButton.addEventListener("click", () => {
@@ -60,23 +47,14 @@ resetButton.addEventListener("click", () => {
   totalAmountElement.textContent = "$0.00";
 });
 
-function setActiveContainer(mainContainer, otherContainers) {
-  if (!mainContainer.classList.contains("input__container--active")) {
-    mainContainer.classList.add("input__container--active");
-    otherContainers.forEach((otherContainer) => {
-      otherContainer.classList.remove("input__container--active");
-    });
-  }
-}
-
-billContainer.addEventListener("click", () => {
-  setActiveContainer(billContainer, [customInput, peopleContainer]);
+containers.forEach((container) => {
+  container.addEventListener("click", (event) => {
+    containers.forEach((c) => c.classList.remove("input__container--active"));
+    container.classList.add("input__container--active");
+    event.stopPropagation();
+  });
 });
 
-customInput.addEventListener("click", () => {
-  setActiveContainer(customInput, [billContainer, peopleContainer]);
-});
-
-peopleContainer.addEventListener("click", () => {
-  setActiveContainer(peopleContainer, [billContainer, customInput]);
+document.addEventListener("click", () => {
+  containers.forEach((container) => container.classList.remove("input__container--active"));
 });
