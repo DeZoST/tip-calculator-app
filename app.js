6aad6572789e4106f5a177626bf371f2ef5ce662
DeZoST <zosted62400@gmail.com>
2023-09-28 20:04:55 +0200

    Modifié les regex pour error state

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+const inputFields = document.querySelectorAll(".input__field");
+const tipButtons = document.querySelectorAll(".tip__button");
+const inputContainers = document.querySelectorAll(".input__container");
 const billInput = document.getElementById("bill__input");
 const customInput = document.getElementById("custom__input");
 const peopleInput = document.getElementById("people__input");
 const tipAmountElement = document.getElementById("tip__amount");
 const totalAmountElement = document.getElementById("total__amount");
 const resetButton = document.getElementById("reset__button");
-const tipButtons = document.querySelectorAll(".tip__button");
-const containers = document.querySelectorAll(".input__container");
-
 let selectedTipPercentage = 0;
+
+function containsAnyLetters(value) {
+  return /[^\d.,]/.test(value);
+}
+
+function resetInputContainers() {
+  inputContainers.forEach((container) => {
+    container.classList.remove("input__container--active");
+    container.classList.remove("input__container--error");
+  });
+  customInput.classList.remove("input__container--error");
+}
+
+function handleInputChange(input, container) {
+  if (containsAnyLetters(input.value)) {
+    container.classList.remove("input__container--active");
+    container.classList.add("input__container--error");
+    input.value = "";
+  } else {
+    container.classList.remove("input__container--error");
+    calculateTip();
+  }
+}
+
+function handleTipButtonClick(button) {
+  tipButtons.forEach((btn) => btn.classList.remove("active"));
+  button.classList.add("active");
+  selectedTipPercentage = parseFloat(button.textContent);
+  customInput.value = "";
+  calculateTip();
+}
 
 function calculateTip() {
   const billAmount = parseFloat(billInput.value) || 0;
@@ -23,38 +54,44 @@ function calculateTip() {
   totalAmountElement.textContent = `$${totalAmount.toFixed(2)}`;
 }
 
-function handleTipButtonClick(button) {
-  tipButtons.forEach((btn) => btn.classList.remove("active"));
-  button.classList.add("active");
-  selectedTipPercentage = parseFloat(button.textContent);
-  customInput.value = "";
-  calculateTip();
-}
+inputFields.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    handleInputChange(input, inputContainers[index]);
+  });
+});
 
-billInput.addEventListener("input", calculateTip);
-customInput.addEventListener("input", calculateTip);
-peopleInput.addEventListener("input", calculateTip);
+customInput.addEventListener("input", () => {
+  handleInputChange(customInput, customInput);
+})
 
 tipButtons.forEach((button) => {
   button.addEventListener("click", () => handleTipButtonClick(button));
 });
 
 resetButton.addEventListener("click", () => {
-  billInput.value = "";
-  peopleInput.value = "";
+  inputFields.forEach((input, index) => {
+    input.value = "";
+    inputContainers[index].classList.remove("input__container--error");
+  });
+
+  tipButtons.forEach((button) => {
+    button.classList.remove("active");
+  })
+
   customInput.value = "";
+  resetInputContainers();
   tipAmountElement.textContent = "$0.00";
   totalAmountElement.textContent = "$0.00";
 });
 
-containers.forEach((container) => {
+inputContainers.forEach((container) => {
   container.addEventListener("click", (event) => {
-    containers.forEach((c) => c.classList.remove("input__container--active"));
+    resetInputContainers();
     container.classList.add("input__container--active");
     event.stopPropagation();
   });
 });
 
 document.addEventListener("click", () => {
-  containers.forEach((container) => container.classList.remove("input__container--active"));
+  resetInputContainers();
 });
